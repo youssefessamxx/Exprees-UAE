e8782bcd46bac048c6d7a50bdf9a4292fbe778f8
youssefessamxx <youssefelbokl100100Gmail.com>
2024-09-27 19:46:55 +0300

    change quotation btn

--- src/components/QuotationBtn.jsx
+++ src/components/QuotationBtn.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function QuotationBtn() {
+  const [showDiv, setShowDiv] = useState(false);
+
+  const handleScroll = () => {
+    const scrollY = window.scrollY;
+    if (scrollY > 900) {
+      setShowDiv(true);
+    } else {
+      setShowDiv(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="bg-[#F05B1F] font-semibold px-6 py-2 rounded-full cursor-pointer md:mb-0 mb-2 inline-block fixed right-[5%] bottom-[5%] text-white ">
-      <Link to="quotation">Get Quotation</Link>
+    <div>
+      {showDiv && (
+        <div className="bg-[#F05B1F] font-semibold px-6 py-2 rounded-full cursor-pointer md:mb-0 mb-2 inline-block fixed right-[5%] bottom-[5%] text-white ">
+          <Link to="quotation">Get Quotation</Link>
+        </div>
+      )}
     </div>
   );
 }
