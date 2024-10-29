19dfb5a2e865a2020eccd9cc4efbcf1ab5ae7a01
youssef-mohamed-07 <youssefmohamedahmed2004.com>
2024-10-29 22:26:17 +0300

    oh

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-// vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()], // Adding React plugin
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return 'vendor'
+            return 'vendor';
           }
         },
       },
     },
     chunkSizeWarningLimit: 1000, // Optional, adjust as necessary
   },
-})
+});
